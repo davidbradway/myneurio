7485685123188f489ada457f808a339fd98caf4b
David Bradway <david.bradway@gmail.com>
2018-12-13 17:13:19 -0500

    capture WIP updates

--- README.md
+++ README.md
@@ -6,9 +6,18 @@ http://api-docs.neur.io
 
 ## Setup
 
-`conda env create -f environment.yml`
+### With Conda
 
+`conda env create -f environment.yml`  
 `conda activate myneurio`
+
+### With venv and pip
+
+`python3 -m venv .venv`  
+`source .venv/bin/activate`  
+`pip install -r requirements.txt`  
+`...`  
+`deactivate`
 
 ## Manually edit your `my_keys.py` file to add your keys and credentials and do not add those secrets to the repo!
 
@@ -16,7 +25,6 @@ http://api-docs.neur.io
 
 ## Usage
 
-`python example.py`
-
+`python example.py`  
 `python read_local.py`
 
--- read_local.py
+++ read_local.py
@@ -1,7 +1,12 @@
 from __future__ import print_function
-import neurio
+from gpiozero import Button
+from signal import pause
 import my_keys
+import neurio
 import time
+
+button = Button(18)
+button.when_pressed = detect
 
 # Setup authentication:
 tp = neurio.TokenProvider(key=my_keys.key, secret=my_keys.secret)
@@ -10,13 +15,15 @@ nc = neurio.Client(token_provider=tp)
 # Get user information (including sensor ID and location ID)
 user_info = nc.get_user_information()
 
-# while loop every second
-try:
-    while True:
-        sample = nc.get_local_current_sample(user_info['locations'][0]['sensors'][0]['ipAddress'])
-        print(sample['timestamp'])
-        print(sample['channels'][2]['p_W'])
-        time.sleep(1.0 - (time.time() % 1.0))
-except KeyboardInterrupt:
-    pass
+def detect():
+    # while loop every second
+    try:
+        for i in range(120):
+            sample = nc.get_local_current_sample(user_info['locations'][0]['sensors'][0]['ipAddress'])
+            print(sample['timestamp'])
+            print(sample['channels'][2]['p_W'])
+            time.sleep(1.0 - (time.time() % 1.0))
+    except KeyboardInterrupt:
+        return
 
+pause()
