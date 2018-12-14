871472c5047ba56d4cefb720bb533c813bcf72f6
David Bradway <david.bradway@gmail.com>
2018-12-13 21:29:45 -0500

    add argument to flask route

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/<label>')
+def hello_world(label):
+    return 'Hello, %s' % label
