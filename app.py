ffc73d72962bb8ddd5f9a39adff30a5be78b3a33
David Bradway <david.bradway@gmail.com>
2018-12-20 17:09:39 -0500

    change save dir

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import datetime
 import read_local
 app = Flask(__name__)
 
+data_dir = "data"
+
+
 @app.route('/<label>')
 def write_file(label):
     datastring = read_local.detect()
-    with open('%s_%s.json' % (label, datetime.datetime.now()), 'w') as f:
+    with open('%s/%s_%s.json' % (data_dir, label, datetime.datetime.now()), 'w') as f:
         f.write(datastring)
     return 'wrote %s \n %s' % (label, datastring)
