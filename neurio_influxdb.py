a616a3c948c09c5640067882a86c77a620476d4d
David Bradway <david.bradway@gmail.com>
2019-01-29 17:02:56 -0500

    add influx database to store timeseries neurio data

--- neurio_influxdb.py
+++ neurio_influxdb.py
@@ -1,30 +1,40 @@
 from __future__ import print_function
 from influxdb import InfluxDBClient
+import my_keys
+import neurio
 import time
+import json
+import sys
 
+
+# Setup authentication:
+tp = neurio.TokenProvider(key=my_keys.key, secret=my_keys.secret)
+# Create client that can authenticate itself:
+nc = neurio.Client(token_provider=tp)
+# Get user information (including sensor ID and location ID)
+user_info = nc.get_user_information()
 
 if __name__ == "__main__":
-    i=1.1
     try:
         print("Loop started")
         client = InfluxDBClient('localhost', 8086, 'admin', 'admin', 'hackaday')
         client.create_database('hackaday')
         while(True):
+            sample = nc.get_local_current_sample(user_info['locations'][0]['sensors'][0]['ipAddress'])
+            i = sample['channels'][2]['p_W']
+            print(i)
             json_body = [
                 {
-                    "measurement": "sensorData",
+                    "measurement": "neurioData",
                     "tags": {
-                        "source": "bedroom",
-                        "type": "temperature"
+                        "source": "Ch2",
+                        "type": "p_W"
                     },
                     "fields": {
                         "value": i
                     }
                 }
             ]
-            i=i*1.1
-            if(i >= 100.0):
-                i=1.1
             client.write_points(json_body)
             #result = client.query('select value from sensorData;')
             #print("Result: {0}".format(result))
