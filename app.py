33193b333fea58ac97803a2ecce152035303cbca
David Bradway <david.bradway@gmail.com>
2019-01-28 21:48:13 -0500

    improve flask routes

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ data_dir = "data"
 
 @app.route('/')
 def root():
-    return "enter label in URL, e.g.: <a href='http://192.168.1.6:5000/dryer'></a>"
+    return "enter label in URL, e.g.: <a href='http://192.168.1.6:5000/dryer'>192.168.1.6:5000/dryer</a>"
 
 
 @app.route('/<label>')
 def write_file(label):
-    if "favicon" not in label:
-        datastring = read_local.detect()
-        with open('%s/%s_%s.json' % (data_dir, label, datetime.datetime.now()), 'w') as f:
-            f.write(datastring)
-        return 'wrote %s \n %s' % (label, datastring)
+    if 'favicon' in label:
+        return 'ignore'
+    datastring = read_local.detect()
+    with open('%s/%s_%s.json' % (data_dir, label, datetime.datetime.now()), 'w') as f:
+        f.write(datastring)
+    return 'wrote %s \n %s' % (label, datastring)
