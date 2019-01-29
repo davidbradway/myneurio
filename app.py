1f02558d524a4423202929235c6a1e9b34ffd41f
David Bradway <david.bradway@gmail.com>
2019-01-28 21:36:08 -0500

    add root route and fix favicon bug

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ app = Flask(__name__)
 data_dir = "data"
 
 
+@app.route('/')
+def root():
+    return "enter label in URL, e.g.: <a href='http://192.168.1.6:5000/dryer'></a>"
+
+
 @app.route('/<label>')
 def write_file(label):
-    datastring = read_local.detect()
-    with open('%s/%s_%s.json' % (data_dir, label, datetime.datetime.now()), 'w') as f:
-        f.write(datastring)
-    return 'wrote %s \n %s' % (label, datastring)
+    if "favicon" not in label:
+        datastring = read_local.detect()
+        with open('%s/%s_%s.json' % (data_dir, label, datetime.datetime.now()), 'w') as f:
+            f.write(datastring)
+        return 'wrote %s \n %s' % (label, datastring)
