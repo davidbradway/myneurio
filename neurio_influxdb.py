a4484cf181eb5f4fd549d3fa5c5911d476e657d0
David Bradway <david.bradway@gmail.com>
2019-01-31 20:48:30 -0500

    add watchdog timer to api calls

--- neurio_influxdb.py
+++ neurio_influxdb.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from influxdb import InfluxDBClient
+from watchdog import Watchdog
 import my_keys
 import neurio
 import time
@@ -7,20 +8,23 @@ import json
 import sys
 
 
-# Setup authentication:
-tp = neurio.TokenProvider(key=my_keys.key, secret=my_keys.secret)
-# Create client that can authenticate itself:
-nc = neurio.Client(token_provider=tp)
-# Get user information (including sensor ID and location ID)
-user_info = nc.get_user_information()
-
 if __name__ == "__main__":
     try:
+        # Setup authentication:
+        tp = neurio.TokenProvider(key=my_keys.key, secret=my_keys.secret)
+        # Create client that can authenticate itself:
+        nc = neurio.Client(token_provider=tp)
+        # Get user information (including sensor ID and location ID)
+        user_info = nc.get_user_information()
+
+        client = InfluxDBClient('localhost', 8086, 'admin', 'admin', 'hackaday')
+        client.create_database('hackaday')
+
+        watchdog = Watchdog(5)
         print("Loop started")
         while(True):
+            watchdog.reset()
             try:
-                client = InfluxDBClient('localhost', 8086, 'admin', 'admin', 'hackaday')
-                client.create_database('hackaday')
                 sample = nc.get_local_current_sample(user_info['locations'][0]['sensors'][0]['ipAddress'])
                 i = sample['channels'][2]['p_W']
                 print(i)
@@ -39,8 +43,8 @@ if __name__ == "__main__":
                 client.write_points(json_body)
                 #result = client.query('select value from sensorData;')
                 #print("Result: {0}".format(result))
-            except:
-                pass
+            except Exception:
+                print("killed")
             time.sleep(1)
     except (KeyboardInterrupt, SystemExit):
         print("\n\nExiting")
