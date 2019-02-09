057ca5d8c1f28406a50f5bda7778609e847e8620
David Bradway <david.bradway@gmail.com>
2019-02-09 15:04:44 -0500

    add thread timeout

--- neurio_influxdb.py
+++ neurio_influxdb.py
@@ -1,8 +1,31 @@
 from __future__ import print_function
 from influxdb import InfluxDBClient
+import threading_timer_decorator_exit
 import my_keys
 import neurio
 import time
+
+@threading_timer_decorator_exit.exit_after(5)
+def slow_calls():
+    sample = nc.get_local_current_sample(user_info['locations'][0]['sensors'][0]['ipAddress'])
+    i = sample['channels'][2]['p_W']
+    print(i, end=' ', flush=True)
+    json_body = [
+                 {
+                  "measurement": "neurioData",
+                  "tags": {
+                   "source": "Ch2",
+                   "type": "p_W"
+                  },
+                  "fields": {
+                   "value": i
+                  }
+                 }
+                ]
+    client.write_points(json_body)
+    #result = client.query('select value from sensorData;')
+    #print("Result: {0}".format(result))
+
 
 if __name__ == "__main__":
     try:
@@ -16,31 +39,12 @@ if __name__ == "__main__":
         client = InfluxDBClient('localhost', 8086, 'admin', 'admin', 'hackaday')
         client.create_database('hackaday')
 
-        #watchdog = Watchdog(5)
         print("Loop started")
         while(True):
-            #watchdog.reset()
             try:
-                sample = nc.get_local_current_sample(user_info['locations'][0]['sensors'][0]['ipAddress'])
-                i = sample['channels'][2]['p_W']
-                print(i, end=' ', flush=True)
-                json_body = [
-                    {
-                        "measurement": "neurioData",
-                        "tags": {
-                            "source": "Ch2",
-                            "type": "p_W"
-                        },
-                        "fields": {
-                            "value": i
-                        }
-                    }
-                ]
-                client.write_points(json_body)
-                #result = client.query('select value from sensorData;')
-                #print("Result: {0}".format(result))
-            except Exception:
-                print(Exception)
+                slow_calls()
+            except (KeyboardInterrupt, Exception) as error:
+                print(error)
             time.sleep(1)
     except (KeyboardInterrupt, SystemExit):
         print("\n\nExiting")
